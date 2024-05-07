a120f09608a622d0762ce2aa7a709ccbef5c2d3c
Cory O'Daniel <cory@coryodaniel.com>
2024-05-07 12:16:05 -0700

    Update to support 15+, 16+ (#15)
    * Update to support 15+, 16+
    
    Includes instance class map and updated script to buildm dropdowns
    
    * rebased + new versions
    
    * add warning to massdriver.yaml
    
    * adding a codeowners file

--- hack/update-instance-classes.rb
+++ hack/update-instance-classes.rb
@@ -8,17 +8,18 @@ $force = false
 $engine = "aurora-postgresql"
 $region = "us-east-1"
 $engines_file = "/tmp/aws-rds-#{$engine}-engines.json"
-# db instance name to ec2 details
+
+# Map of instance class name to instance details
 $instance_classes = {}
 
-# tracks group id to list of aws instance classes
+# tracks JSON Schema group id to list of aws instance classes
 $instances_classes_in_group = {}
 
 # This is an optimization around minimizing the number of oneOfs w/ repeated instance classes
 engine_version_to_instance_class_group = {}
 
 def generate_instance_class_group_jsonschema_id(instance_classes)
-  group_id = Digest::SHA2.hexdigest(instance_classes.sort.join(','))
+  group_id = Digest::SHA2.hexdigest(instance_classes.uniq.sort.join(','))
 
   $instances_classes_in_group[group_id] = instance_classes
 
@@ -29,7 +30,7 @@ $mdyaml = "massdriver.yaml"
 conf = YAML.load(File.read($mdyaml))
 
 def maybe_get_data(cmd, file, force)
-  if !File.exists?(file) || force
+  if !File.exist?(file) || force
     `#{cmd} > #{file}`
   else
     puts "#{file} exists, skipping fetch..."
@@ -93,7 +94,6 @@ supported_engine_versions.each do |engine_version|
   instances["OrderableDBInstanceOptions"].each do |v|
     next if !supported_engine_versions.member?(v["EngineVersion"])
     rds_instance_class = v["DBInstanceClass"]
-
     supported_engine_versions_to_instance_class_map[engine_version].push(rds_instance_class)
 
     if rds_instance_class == "db.serverless"
@@ -116,6 +116,7 @@ supported_engine_versions.each do |engine_version|
   engine_version_to_instance_class_group[engine_version] = group_id
 end
 
+# Get instance details, only 100 at a time supported by API
 $instance_classes.each_slice(100) do |instance_class_chunk|
   chunked_ec2_names = instance_class_chunk.map{|_k, v| v[:EC2InstanceType]}
   joined_for_cli = chunked_ec2_names.join(' ')
@@ -126,26 +127,25 @@ $instance_classes.each_slice(100) do |instance_class_chunk|
   details = read_json_file(file)
 
   details["InstanceTypes"].each do |deets|
-    if deets["CurrentGeneration"]
-      rds_instance_class = transform_instance_class_from(deets["InstanceType"], :ec2)
-      gib = deets["MemoryInfo"]["SizeInMiB"] / 1_024
-      vcpus = deets["VCpuInfo"]["DefaultVCpus"]
+    rds_instance_class = transform_instance_class_from(deets["InstanceType"], :ec2)
+    gib = deets["MemoryInfo"]["SizeInMiB"] / 1_024
+    vcpus = deets["VCpuInfo"]["DefaultVCpus"]
 
-      note = if deets["InstanceType"].start_with?("t")
-        "Burstable"
-      else
-        "Memory Optimized"
-      end
-
-      $instance_classes[rds_instance_class].merge!({
-        RDSInstanceType: rds_instance_class,
-        EC2InstanceType: deets["InstanceType"],
-        DefaultVCpus: vcpus,
-        SizeInGiB: gib,
-        Note: note,
-        Label: "#{note} #{gib} GiB, #{vcpus} vCPUs (#{rds_instance_class})"
-      })
+    note = if deets["InstanceType"].start_with?("t")
+      "Burstable"
+    else
+      "Memory Optimized"
     end
+
+    $instance_classes[rds_instance_class].merge!({
+      CurrentGeneration: deets["CurrentGeneration"],
+      RDSInstanceType: rds_instance_class,
+      EC2InstanceType: deets["InstanceType"],
+      DefaultVCpus: vcpus,
+      SizeInGiB: gib,
+      Note: note,
+      Label: "#{note} #{gib} GiB, #{vcpus} vCPUs (#{rds_instance_class})"
+    })
   end
 end
 
@@ -153,10 +153,10 @@ end
 conf["params"]["$defs"] = {}
 
 engine_version_to_instance_class_group.values.uniq.each do |group_id|
-  formatted_instance_classes = $instances_classes_in_group[group_id].
-    map {|rds_instance_class| $instance_classes[rds_instance_class]}.
-    sort { |a,b| [a[:DefaultVCpus], a[:SizeInGiB]] <=> [b[:DefaultVCpus] , b[:SizeInGiB]]}.
-    map {|ic| {"title" => ic[:Label], "const" => ic[:RDSInstanceType]}}
+  formatted_instance_classes = $instances_classes_in_group[group_id].uniq.
+    map { |rds_instance_class| $instance_classes[rds_instance_class]}.
+    sort { |a,b| [a[:DefaultVCpus], a[:SizeInGiB]] <=> [b[:DefaultVCpus] , b[:SizeInGiB]] }.
+    map { |ic| {"title" => ic[:Label], "const" => ic[:RDSInstanceType]}}
 
   conf["params"]["$defs"]["instance-class-group-#{group_id}"] = {
     "title" => "Instance Class",
